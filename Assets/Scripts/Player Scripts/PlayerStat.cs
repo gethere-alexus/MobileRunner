97d2e4e57320c9a21e1899ea81995e86aec2151a
O.Ovchynnikov <owalolzzz@gmail.com>
2024-01-27 09:14:51 +0000

    Boost mechanic was completely remade, making the system flexible to edit

--- Assets/Scripts/Player Scripts/PlayerStat.cs
+++ Assets/Scripts/Player Scripts/PlayerStat.cs
@@ -1,13 +1,32 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public abstract class PlayerStat : MonoBehaviour, IBoostable
 {
-    [SerializeField] private BoostType _appliableBoost;
-    public abstract void IncrementBoostingValue(int amountToAdd);
-    public abstract void DecrementBoostingValue(int amountToTake);
-    public abstract int PreviewValueAfterApplying(int amountToApply);
-    public abstract int PreviewValueAfterRemoving(int amountToApply);
+    [SerializeField] private BoostDataContainer _appliableBoost;
+    [SerializeField] protected int _value, _maxValue;
+    [SerializeField] protected int _defaultValue, _defaultMaxValue;
+
+    private void Awake()
+    {
+        if(_value > _maxValue ) _value = _maxValue;
+        
+        _defaultValue = _value;
+        _defaultMaxValue = _maxValue;
+    }
+
+    public virtual void IncrementBoostingValue(int amountToAdd) => _value += amountToAdd;
+
+    public virtual void DecrementBoostingValue(int amountToTake)
+    {
+        if (_value - amountToTake > 0)
+        {
+            _value -= amountToTake;
+        }
+    }
+    public virtual int PreviewValueAfterApplying(int amountToApply) => _value + amountToApply;
+    public virtual int PreviewValueAfterRemoving(int amountToApply) => _value - amountToApply;
     
-    public BoostType AppliableBoost => _appliableBoost;
+    public BoostDataContainer AppliableBoost => _appliableBoost;
+    public virtual int BoostableValue => _value;
+    public virtual int BoostImpact => _value - _defaultValue;
 }
--- Assets/Scripts/UI Scripts/StatsPopUpInstantiator.cs
+++ Assets/Scripts/UI Scripts/StatsPopUpInstantiator.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class StatsPopUpInstantiator : PopUpInstantiator
 {
-    
+    [SerializeField] private PlayerStatsRepresenter _playerStatsRepresenter;
     public override void InstantiatePopUp()
     {
         StatsPopUp popUpConf = Instantiate(_popUpPrefab).GetComponent<StatsPopUp>();
-        popUpConf.Configure();
+        popUpConf.Configure(_playerStatsRepresenter.GetBoostImpacts(), _playerStatsRepresenter.GetBoostableValues());
     }
 }
